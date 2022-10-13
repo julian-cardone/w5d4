789bf9e446c97e52375016d8606c4bbb2474303d
jkhbuild <jkh.build@gmail.com>
2022-10-13 17:33:30 -0400

    user model started

--- URLShortener/db/migrate/20221013210957_user.rb
+++ URLShortener/db/migrate/20221013210957_user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :email, null: false, unique: true
-      t.string :username, null: false, unique: true
+      t.string :email, null: false, index: { unique: true }
 
       t.timestamps
     end
--- URLShortener/db/schema.rb
+++ URLShortener/db/schema.rb
@@ -16,9 +16,9 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_13_210957) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
-    t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
